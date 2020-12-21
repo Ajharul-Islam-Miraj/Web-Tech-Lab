b17b553b7ef660a50f029b44a585e9570446f7d2
mirajcs18 <miraj.cs18@gmail.com>
2020-12-21 22:10:40 +0600

    Quiz 2 alternative assignment done

---
+++ Final Term/QuizAssignment/views/AllBooks.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Book - Searching </title>
+    </head>
+    <body>
+        <center>
+            <table>
+                <tr>
+                    <td>Search:</td>
+                    <td><input type="text" name="searchBox" id="searchBox" placeholder="Search" onkeyup="DoSearch(this)"></td>
+                </tr>
+            </table>
+            <div id="searching"><!--JS VALUE --></div>
+        </center>
+    </body>
+    <script src="../scripts/Searching.js"></script>
+</html>
